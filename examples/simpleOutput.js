59434ba0f66955b4120ea460c44c7d1fb565e27e
Paul Hopton <paul@relayr.de>
2015-02-27 00:11:25 +0100

    cleaned up example

--- examples/simpleOutput.js
+++ examples/simpleOutput.js
@@ -1,16 +1,16 @@
 var Relayr = require("../");
 
-relayr = new Relayr("0e5b71a9-ad78-4e24-9ec1-8b3de04ff710");
+var app_id = "0e5b71a9-ad78-4e24-9ec1-8b3de04ff710";
+var token =  "BwwORoJcI9iKDN9NEHs1hXYOeCV3cEoS";
+var htu =  "6fbb27dd-ac8f-4d1f-bd82-8d37405d8cfb";
+var snd =  "a0dde2dc-bce6-4df3-8f6c-2b773ce6fb90";
+relayr = new Relayr(app_id);
 
-var relayrKeys = {
-    dev_id: "6fbb27dd-ac8f-4d1f-bd82-8d37405d8cfb",
-    token:  "BwwORoJcI9iKDN9NEHs1hXYOeCV3cEoS"
-};
 
-relayr.connect(relayrKeys.token, relayrKeys.dev_id);
+relayr.connect(token, htu);
 
 relayr.on('connect', function () {
-    relayr.connect(relayrKeys.token, "a0dde2dc-bce6-4df3-8f6c-2b773ce6fb90");
+    relayr.connect(token, snd);
 });
 relayr.on('data', function (topic, msg) {
     console.log("TOPIC:"+topic);
