7b05c5765ce69edfcf2551731ff7a01af50af75d
Paul Hopton <paul@relayr.de>
2015-02-27 00:02:10 +0100

    getting better, but we now have two connecttions

--- examples/simpleOutput.js
+++ examples/simpleOutput.js
@@ -1,19 +1,20 @@
-var relayr = require("../");
+var Relayr = require("../");
+
+relayr = new Relayr("0e5b71a9-ad78-4e24-9ec1-8b3de04ff710");
 
 var relayrKeys = {
-    app_id: "0e5b71a9-ad78-4e24-9ec1-8b3de04ff710",
     dev_id: "6fbb27dd-ac8f-4d1f-bd82-8d37405d8cfb",
     token:  "BwwORoJcI9iKDN9NEHs1hXYOeCV3cEoS"
 };
 
-relayr.connect(relayrKeys);
+relayr.connect(relayrKeys.token, relayrKeys.dev_id);
+relayr.connect(relayrKeys.token, "a0dde2dc-bce6-4df3-8f6c-2b773ce6fb90");
 
-relayr.listen(function(err,data){
-    //fires for every sensor event 
-    if (err) {
-        console.log("Oh No!", err)
-    } else {
-        console.log(data);
-    }
+relayr.on('connect', function () {
+    console.log("hell yeah");
+});
+relayr.on('data', function (topic, msg) {
+    console.log("TOPIC:"+topic);
+    console.log(msg);
 });
 
