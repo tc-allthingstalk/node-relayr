f5c81d14a9426897b62db80ff7b7a22a5904b086
Paul Hopton <paul@relayr.de>
2015-02-27 11:52:13 +0100

    added 2 more devices to demo

--- examples/simpleOutput.js
+++ examples/simpleOutput.js
@@ -1,16 +1,22 @@
+/*
+ * run with bunyan to et sensible logging:
+ * node examples/simpleOutput.js | bunyan -o short
+ */
 var Relayr = require("../");
 
 var app_id = "0e5b71a9-ad78-4e24-9ec1-8b3de04ff710";
 var token =  "BwwORoJcI9iKDN9NEHs1hXYOeCV3cEoS";
 var htu =  "6fbb27dd-ac8f-4d1f-bd82-8d37405d8cfb";
 var snd =  "a0dde2dc-bce6-4df3-8f6c-2b773ce6fb90";
+var lcp =  "a2242103-c522-4b6f-a533-7740db2cb891";
 relayr = new Relayr(app_id);
 
 
 relayr.connect(token, htu);
+relayr.connect(token, snd);
+relayr.connect(token, lcp);
 
 relayr.on('connect', function () {
-    relayr.connect(token, snd);
 });
 relayr.on('data', function (topic, msg) {
     console.log("TOPIC:"+topic);
