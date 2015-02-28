4f559df284701548cd93e59edb80b2090516a7f3
Paul Hopton <paul@relayr.de>
2015-02-28 12:12:26 +0100

    removed deliberate debuggin error from example

--- examples/simpleOutput.js
+++ examples/simpleOutput.js
@@ -8,7 +8,7 @@ var app_id = "0e5b71a9-ad78-4e24-9ec1-8b3de04ff710";
 var token =  "BwwORoJcI9iKDN9NEHs1hXYOeCV3cEoS";
 var htu =  "6fbb27dd-ac8f-4d1f-bd82-8d37405d8cfb";
 var snd =  "a0dde2dc-bce6-4df3-8f6c-2b773ce6fb90";
-var lcp =  "a2242103-c522-4b6f-a533-7740db2cb891x";
+var lcp =  "a2242103-c522-4b6f-a533-7740db2cb891";
 relayr = new Relayr(app_id);
 
 relayr.deviceModel(token, htu, function (err, description) {
