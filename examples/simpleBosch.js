44e516668af777348e5ab32c3cceb1310b23bd86
Paul Hopton <paul@relayr.de>
2015-02-28 11:22:36 +0100

    added deviceModel Printing to example

--- examples/simpleBosch.js
+++ examples/simpleBosch.js
@@ -5,6 +5,11 @@ var dev_id = "8736887d-7661-4501-a4a6-c8a42ce461ba";
 var token  = "BwwORoJcI9iKDN9NEHs1hXYOeCV3cEoS";
 var relayr = new Relayr(app_id);
 
+relayr.deviceModel(token, dev_id, function (err, description) {
+    console.log("-------------- BSH --------------------");
+    console.log(description);
+    console.log("----------------------------------------------------------");
+});
 
 relayr.connect(token, dev_id);
 
